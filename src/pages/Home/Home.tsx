abbd8c9013f356b36ff9ed9625f6af1d426a6f1c
Lucas Felix <lucariozin.net@gmail.com>
2023-02-24 15:48:32 -0300

    configuring 'axios api service'.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
+
+import { api } from 'src/services/api'
+
 import { UserSummary } from '@components/UserSummary'
 import { PublicationList } from '@components/PublicationList'
 import { FindPublications } from '@components/FindPublications'
 
 export const Home = () => {
+  useEffect(() => {
+    api.get('/users/Lucariozin').then((response) => console.log(response))
+  }, [])
+
   return (
     <>
       <UserSummary />
