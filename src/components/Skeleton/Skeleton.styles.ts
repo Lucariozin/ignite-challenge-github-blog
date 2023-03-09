e93e0d4f435adf15992ae010edabda2f0850fab2
Lucas Felix <lucariozin.net@gmail.com>
2023-03-09 10:08:21 -0300

    adding a 'responsiveness feature' to the 'Skeleton' component.

--- src/components/Skeleton/Skeleton.styles.ts
+++ src/components/Skeleton/Skeleton.styles.ts
@@ -1,6 +1,52 @@
+import { CSSProperties } from 'react'
+
 import styled from 'styled-components'
 
-export const Container = styled.div`
+const formatCssObject = (cssObj?: CSSProperties) => {
+  return (
+    JSON.stringify(cssObj ?? '')
+      .replace('{', '')
+      .replace('}', '')
+      .replaceAll('"', '')
+      .replaceAll('A', '-a')
+      .replaceAll('B', '-b')
+      .replaceAll('C', '-c')
+      .replaceAll('D', '-d')
+      .replaceAll('E', '-e')
+      .replaceAll('F', '-f')
+      .replaceAll('G', '-g')
+      .replaceAll('H', '-h')
+      .replaceAll('I', '-i')
+      .replaceAll('J', '-j')
+      .replaceAll('K', '-k')
+      .replaceAll('L', '-l')
+      .replaceAll('M', '-m')
+      .replaceAll('N', '-n')
+      .replaceAll('O', '-o')
+      .replaceAll('P', '-p')
+      .replaceAll('Q', '-q')
+      .replaceAll('R', '-r')
+      .replaceAll('S', '-s')
+      .replaceAll('T', '-t')
+      .replaceAll('U', '-u')
+      .replaceAll('V', '-v')
+      .replaceAll('W', '-w')
+      .replaceAll('X', '-x')
+      .replaceAll('Y', '-y')
+      .replaceAll('Z', '-z')
+      .replaceAll(',', ' !important;') + ' !important;'
+  )
+}
+
+interface ContainerProps {
+  mediaQueries?: {
+    md?: CSSProperties
+    hmd?: CSSProperties
+    sm?: CSSProperties
+  }
+}
+
+export const Container = styled.div<ContainerProps>`
   @keyframes Skeleton {
     0% {
       background-color: ${({ theme }) => theme.palette.gray[600]};
@@ -13,6 +59,18 @@ export const Container = styled.div`
     100% {
       background-color: ${({ theme }) => theme.palette.gray[600]};
     }
+  }
+
+  ${({ theme, mediaQueries }) => mediaQueries?.md && theme.breakpoints.down('md')} {
+    ${({ mediaQueries }) => formatCssObject(mediaQueries?.md)}
+  }
+
+  ${({ theme, mediaQueries }) => mediaQueries?.hmd && theme.breakpoints.down(750)} {
+    ${({ mediaQueries }) => formatCssObject(mediaQueries?.hmd)}
+  }
+
+  ${({ theme, mediaQueries }) => mediaQueries?.sm && theme.breakpoints.down('sm')} {
+    ${({ mediaQueries }) => formatCssObject(mediaQueries?.sm)}
   }
 
   border-radius: 2px;
--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -2,8 +2,15 @@ import type { CSSProperties } from 'react'
 
 import { Container } from './Skeleton.styles'
 
-interface SkeletonProps extends CSSProperties {}
+interface SkeletonProps extends CSSProperties {
+  style?: CSSProperties
+  mediaQueries?: {
+    md?: CSSProperties
+    hmd?: CSSProperties
+    sm?: CSSProperties
+  }
+}
 
-export const Skeleton = (props: SkeletonProps) => {
-  return <Container style={{ ...props }} />
+export const Skeleton = ({ style, mediaQueries, ...props }: SkeletonProps) => {
+  return <Container mediaQueries={mediaQueries} style={{ ...props, ...style }} />
 }
